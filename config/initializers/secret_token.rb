1d0d5d56a4ac78b73d116da6b05931c711b0bcc2
Michael Ward <m.ward@leythers.com>
2011-10-04 20:22:10 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingPlanner::Application.config.secret_token = '09abc9c3d2613771097ae101ee960c418e8e20857fbaa0eb486139f2219e204c776d308fe5475db60363217ebe047e30c807971e88a4d0a5693cd8522eedccdd'
