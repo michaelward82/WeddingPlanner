7255c99889d38453f109b927c1d4864f359e3c74
Michael Ward <m.ward@leythers.com>
2011-10-10 03:50:26 +0100

    Added confirmation to delete action

--- app/assets/javascripts/nested_form.js
+++ app/assets/javascripts/nested_form.js
@@ -39,13 +39,15 @@ jQuery(function($) {
     return false;
   });
 
-  $('form a.remove_nested_fields').live('click', function() {
-    var hidden_field = $(this).prev('input[type=hidden]')[0];
-    if(hidden_field) {
-      hidden_field.value = '1';
+  $(".remove_nested_fields").live("click", function() {
+    if (confirm("Delete this budget item?")) { 
+      var hidden_field = $(this).prev('input[type=hidden]')[0];
+      if(hidden_field) {
+        hidden_field.value = '1';
+      }
+      $(this).closest('.fields').hide();
+      $(this).closest("form").trigger('nested:fieldRemoved');
     }
-    $(this).closest('.fields').hide();
-    $(this).closest("form").trigger('nested:fieldRemoved');
     return false;
   });
 });
