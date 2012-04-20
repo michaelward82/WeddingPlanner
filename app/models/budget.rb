fd63c9816bb96d6de6cda5e5c792b6d3b96fbfdb
Michael Ward <m.ward@leythers.com>
2012-04-21 00:43:45 +0100

    Fixed validation of blank budget_item rows

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,6 +2,6 @@ class Budget < ActiveRecord::Base
   belongs_to :user
   has_many :budget_items, :order=>"position ASC"
   
-  accepts_nested_attributes_for :budget_items, :allow_destroy => true 
+  accepts_nested_attributes_for :budget_items, :allow_destroy => true, :reject_if => proc { |a| a['description'].blank? }
   attr_accessible :budget_items_attributes
 end
