4cf6c06d3eb8f64ffa88de7fa0fcb9fccd50a3a4
Michael Ward <m.ward@leythers.com>
2012-06-15 16:58:03 +0100

    Adds tests for budget_items model

--- spec/models/budget_item_spec.rb
+++ spec/models/budget_item_spec.rb
@@ -1,6 +1,16 @@
 describe BudgetItem do
-  it "does not validate when there is no data" do
-    budget_item = BudgetItem.new()
-    budget_item.save.should == false
+  before :each do
+    @budget_item = BudgetItem.new :description => "Description", :estimated_cost => 1, :actual_cost => 1, :position => 1
+  end
+
+  describe "#new" do
+    it "takes four parameters and returns a BudgetItem object" do
+      @budget_item.should be_an_instance_of BudgetItem
+    end
+  end
+
+  it "requires presence of description" do
+    @budget_item.description = ""
+    @budget_item.save.should == false
   end
 end
