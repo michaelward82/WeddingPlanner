bc1b409c14243b3e7ef346095c513139a7e8d3bd
Michael Ward <m.ward@leythers.com>
2012-06-15 17:28:57 +0100

    Adds further tests for budget_item model

--- app/models/budget_item.rb
+++ app/models/budget_item.rb
@@ -4,4 +4,5 @@ class BudgetItem < ActiveRecord::Base
   acts_as_list
   
   validates_presence_of :description
+  validates_numericality_of :estimated_cost, :actual_cost
 end
--- spec/models/budget_item_spec.rb
+++ spec/models/budget_item_spec.rb
@@ -3,14 +3,18 @@ describe BudgetItem do
     @budget_item = BudgetItem.new :description => "Description", :estimated_cost => 1, :actual_cost => 1, :position => 1
   end
 
-  describe "#new" do
-    it "takes four parameters and returns a BudgetItem object" do
-      @budget_item.should be_an_instance_of BudgetItem
-    end
-  end
-
   it "requires presence of description" do
     @budget_item.description = ""
-    @budget_item.save.should == false
+    @budget_item.save.should_not == true
+  end
+
+  it "does not validate non numeric values for estimated_cost" do
+    @budget_item.estimated_cost = "NaN"
+    @budget_item.save.should_not == true
+  end
+
+  it "does not validate non numeric values for actual_cost" do
+    @budget_item.actual_cost = "NaN"
+    @budget_item.save.should_not == true
   end
 end
